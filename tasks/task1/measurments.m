46c18194a40be673e0b7c09e75f32cad91d6ae21
MaximilianDio <36553479+MaximilianDio@users.noreply.github.com>
2019-11-05 13:31:56 +0100

    task 1 - matlab: calculation
    matlab measurments and calculation files

---
+++ tasks/task1/measurments.m
@@ -0,0 +1,63 @@
+%% measurments
+%% constants
+
+g = 9.81; %N/m^2
+%% mass
+
+massCar = [1902.84; 1902.33 ; 1902.34]*10^-3; %kg Gesamtmasse Auto ohne Akku
+    
+massAkku = [ 420.35; 420.33 ; 420.34]*10^-3; %kg Gesmatmasse Akku
+
+
+mges = mean(massCar)+mean(massAkku); %kg Gesmatmasse
+%% CM
+
+%% x-direction
+ 
+Fx = [ 8.2 ; 8.2; 8.2]; %N 
+
+
+L = 0.295; %m %Gesmatlänge 
+Lx = 0.0755; %m Länge von Vorderachse zu Kraftangriffspunkt 
+LF = L+Lx; %m Angriffspunkt Kraft F
+
+Lh = mean(Fx)*LF/(mges*g);
+Lv = L-Lh;
+
+%% y-direction
+
+Fy = [9.1; 9.5; 9.4; 9.4; 9.4]; %N %kraft ohne Batterie
+
+b = 0.16; % m Breite Felge-Felge
+
+bF = 0.065+b/2; %m Angriffspunkt von rechter Felge
+
+bAkku = 0.033+b/2; %m Schwerpunkt Akku von rechter Felge
+
+brCar = mean(Fy)*bF/((mean(massCar))*g); %m Schwerpunkt Auto ohne Akku von rechter Felge
+
+br = (mean(massAkku)*bAkku+mean(massCar)*brCar)/mges; %m Schwerpunkt Auto mit Akku von rechter Felge
+
+%% z-direction
+
+Fz = 3.7; %N
+hF = 0.298-0.032; %m
+
+h = hF*Fz/(mges*g); %m Schwerpunkt gesamtmasse Auto mit Akku
+
+%% Massenträgheit
+
+% Aufbau:
+
+LAufbau = 0.518; %m
+
+masseAufbau = (597.4+1206.95)*(10^-3); %kg
+
+FAufbau = 5.65; %N
+LFAufbau = 0.255; %m
+
+zAufbau = LFAufbau *FAufbau/(masseAufbau*g); %m %Schwerpunkt von Drehpunkt
+
+% Schwingung
+%Abstand Drehpunkt zu Schwerpunkt
+LAufbauSchwerpunkt = LAufbau-zAufbau; %m 
