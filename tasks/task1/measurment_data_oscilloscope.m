da26e04245f4ac5f2cccbfcc6c948407c0f03287
paulo-95 <57364899+paulo-95@users.noreply.github.com>
2019-11-05 17:40:41 +0100

    Change in Measurements file

--- tasks/task1/measurment_data_oscilloscope.m
+++ tasks/task1/measurment_data_oscilloscope.m
@@ -6,11 +6,11 @@ clc
 
 %%
 [time, values_without]  = get_oscillatordata("meas1_no_car.csv");
-[time, values_with]  = get_oscillatordata("meas1_with_car.csv");
+[time_new, values_with]  = get_oscillatordata("Meas_car_new.csv");
 
 plot(time(1:end-1),values_without(1:end-1))
 hold on
-plot(time(1:end-1),values_with(1:end-1))
+plot(time_new(1:end-1),values_with(1:end-1))
 
 
 
--- tasks/task1/measurments.m
+++ tasks/task1/measurments.m
@@ -1,11 +1,7 @@
 %% measurments
-close all
-clear
-clc
-
 %% constants
 
-g = 9.81; %m/s^2
+g = 9.81; %N/m^2
 %% mass
 
 massCar = [1902.84; 1902.33 ; 1902.34]*10^-3; %kg Gesamtmasse Auto ohne Akku
@@ -14,15 +10,15 @@ massAkku = [ 420.35; 420.33 ; 420.34]*10^-3; %kg Gesmatmasse Akku
 
 
 mges = mean(massCar)+mean(massAkku); %kg Gesmatmasse
-%% CM
+%% CoM
 
 %% x-direction
  
 Fx = [ 8.2 ; 8.2; 8.2]; %N 
 
 
-L = 0.295; %m %Gesmatlänge 
-Lx = 0.0755; %m Länge von Vorderachse zu Kraftangriffspunkt 
+L = 0.295; %m %Gesmatl?nge 
+Lx = 0.0755; %m L?nge von Vorderachse zu Kraftangriffspunkt 
 LF = L+Lx; %m Angriffspunkt Kraft F
 
 Lh = mean(Fx)*LF/(mges*g);
@@ -49,19 +45,46 @@ hF = 0.298-0.032; %m
 
 h = hF*Fz/(mges*g); %m Schwerpunkt gesamtmasse Auto mit Akku
 
-%% Massenträgheit
+%% Massentr?gheit
 
 % Aufbau:
 
+% Abstand con der Drehachse zum unteren Teil der 'Schwingbuehne'
 LAufbau = 0.518; %m
 
+% Masse der Schwingbuehne
 masseAufbau = (597.4+1206.95)*(10^-3); %kg
 
+% Kraft im statischen Gleichgewicht
 FAufbau = 5.65; %N
+% Hebelarm
 LFAufbau = 0.255; %m
 
-zAufbau = LFAufbau *FAufbau/(masseAufbau*g); %m %Schwerpunkt von Drehpunkt
+% Schwerpunkt der Schwingbuehne dargestellt im Schwingbuehnen KOS
+zAufbau = LFAufbau *FAufbau/(masseAufbau*g); %m
 
-% Schwingung
-%Abstand Drehpunkt zu Schwerpunkt
+% Abstand des Schwerpunktes der Schwingbuehne zur Drehachse
 LAufbauSchwerpunkt = LAufbau-zAufbau; %m 
+
+% Periodendauer der Schwingbuehne
+T_Aufbau = 1.444; %s
+
+%Tr?gheitsmoment (bzgl. Drehachse) berechnet sich aus der Eigenfrequenz der Schwingbuehne(Pendelschwingung)
+I_Aufbau = (masseAufbau*g*LAufbauSchwerpunkt)/((2*pi/T_Aufbau)^2);
+
+% Messung fuer Schingbuehne und Auto
+% Gesamtschwerpunkt im Schwingbuehnen KOS
+z_ges = (masseAufbau * zAufbau + mges * 0.135)/(masseAufbau + mges); % 13.5cm wurden grob gemessen
+
+% Abstand Gesamtschwerpunkt zur Drehachse
+L_CoM_ges = LAufbau - z_ges;
+
+% Periodendauer gesamtsystem
+T_ges = 1.372;
+
+% Abstand von der Drehachse zum Schwerpunkt des Autos
+L_CoM_Auto = LAufbau - 0.135;
+
+I_Auto = ((masseAufbau + mges)*g*L_CoM_ges)/((2*pi/T_ges)^2) - I_Aufbau - mges*L_CoM_Auto^2; %kg m^2
+
+
