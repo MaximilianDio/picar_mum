2620988aba3c004677740bbb62dbc0999987b11b
Timo <timo.schmidt@tuhh.de>
2019-11-08 10:58:30 +0100

    test

--- catkin_ws/src/keyboard_control/src/keyboard_control_node.py
+++ catkin_ws/src/keyboard_control/src/keyboard_control_node.py
@@ -176,7 +176,6 @@ class KeyboardController(object):
             car_cmd.angle = -self.angle # angle will be clamped by the car itself
 
 
-
         """ prints angle and data for debugging """
         #print("angle = " + str(car_cmd.angle))
         #print("velocity = " + str(car_cmd.velocity))
