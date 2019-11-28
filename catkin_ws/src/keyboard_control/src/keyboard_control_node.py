2b59e10fe9c3ec0352c2c9ab9b3d3619e4bdc216
paulo.95 <paul.over@tuhh.de>
2019-11-28 11:07:19 +0100

    commit 123

--- catkin_ws/src/keyboard_control/src/keyboard_control_node.py
+++ catkin_ws/src/keyboard_control/src/keyboard_control_node.py
@@ -33,7 +33,7 @@ class KeyboardController(object):
         vehicle_name = rospy.get_namespace().strip("/")
 
         self.velocity = 0.3
-        self.angle = 16.5  # absolute value
+        self.angle = 100.0  # absolute value
 
         self.screen = pygame.display.set_mode(
             (KeyboardController.SCREEN_SIZE, KeyboardController.SCREEN_SIZE)
@@ -174,6 +174,7 @@ class KeyboardController(object):
             car_cmd.angle = self.angle # angle will be clamped by the car itself
         if keys[self.K_RIGHT]:
             car_cmd.angle = -self.angle # angle will be clamped by the car itself
+
 
 
         """ prints angle and data for debugging """
