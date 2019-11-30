558ce79cf8c37d5a6bb75585b61a78e7349d7dfc
maximilian <maximilian.dio@googlemail.com>
2019-11-30 16:29:48 +0100

    new task leader_follower: initialized package structure of leader_follower.

--- catkin_ws/src/40_leader_follower/leader_detection/src/leader_detection_node.py
+++ catkin_ws/src/40_leader_follower/leader_detection/src/leader_detection_node.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("leader_detection_node")
+
 # TODO: write node for leader detection- see line detection from lane_lane_follower
