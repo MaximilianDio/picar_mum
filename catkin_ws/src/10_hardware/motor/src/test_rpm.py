333f7e803f940983c4579c11dd4a4506b4f65179
paulo-95 <paul.over@tuhh.de>
2020-01-06 17:00:45 +0100

    rpm sensor wheels

--- catkin_ws/src/10_hardware/motor/src/test_rpm.py
+++ catkin_ws/src/10_hardware/motor/src/test_rpm.py
@@ -1,28 +1,49 @@
-import Adafruit_GPIO.I2C as i2c
+#!/usr/bin/env python
+import serial
 import time
 import struct
 import numpy as np
+from picar_msgs.msg import WheelSpeedStamped
+import rospy
 
-ticks_per_sec = 16000000/64.0
+ticks_per_sec = 16000000.0/256.0
 pulses_per_rev = 10
 
 
 class Node(object):
     def __init__(self):
-        self.dev = i2c.get_i2c_device(0x54)
+        self.serial = serial.Serial("/dev/ttyUSB0", 57600, timeout=1.0)
+        time.sleep(2.0)
+        rospy.init_node("rpm")
+        self.publisher = rospy.Publisher(
+            "wheel_speed",
+            WheelSpeedStamped,
+            queue_size=1)
+
+
+
 
 
 if __name__ == "__main__":
     n = Node()
     speeds = [0, 0, 0]
     index = 0
-    while True:
+    data = n.serial.read()
+    print(len(data))
+    while not rospy.is_shutdown():
         time.sleep(0.1)
-        data = n.dev.readList(0, 6)
-        data = struct.unpack('>HH', data)
-        speed = ticks_per_sec/data[1]/pulses_per_rev if data[1] > 0 else 0.0
-        speeds[index] = speed
-        index = index+1 if index < 2 else 0
-        speed = np.median(speeds)
+        n.serial.write(b"\x01")
+        data = n.serial.read(8)
+        print(len(data))
+        data = struct.unpack('>HHHH', data)
+        speed = []
+        for ticks in data:
+            speed.append(ticks_per_sec/ticks/pulses_per_rev*2*3.1416 if ticks > 0 else 0.0)
         print(data)
         print(speed)
+        message = WheelSpeedStamped()
+        message.front_left = speed[0]
+        message.front_right = speed[1]
+        message.rear_left = speed[2]
+        message.rear_right = speed[3]
+        n.publisher.publish(message)
