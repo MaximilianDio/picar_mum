6162fe8abb3cff8c74e38b01db925f5015dac186
paulo-95 <paul.over@tuhh.de>
2020-01-12 16:52:46 +0100

    trys and exepctions included, ready for debugging ..

--- catkin_ws/src/50_project/velocity_estimation/src/velocity_estimation_node.py
+++ catkin_ws/src/50_project/velocity_estimation/src/velocity_estimation_node.py
@@ -4,6 +4,7 @@ from picar_msgs.msg import WheelSpeedStamped
 from std_msgs.msg import Float32
 from velocity_estimator import VelocityEstimator
 from picar.parameters import Wheel
+from geometry_msgs.msg import Quaternion
 
 
 class VelocityEstimation(object):
@@ -19,14 +20,23 @@ class VelocityEstimation(object):
 
     def encoder_callback(self, input_msg):
         velocity = Float32()
-        velocity.data = self.velocity_estimator.getCOMvel([input_msg.rear_left, input_msg.rear_right])
+        try:
+            velocity.data = self.velocity_estimator.getCOMvel([input_msg.rear_left, input_msg.rear_right])
+        except AttributeError:
+            velocity.data = self.velocity_estimator.getCOMvel([input_msg.w, input_msg.w]) # map omega float64 value in both list places
+
+        # velocity.header.stamp = rospy.Time.now()
         self.publishers["velocity_estimated"].publish(velocity)
         self.rate.sleep()
 
     def init_subscribers(self):
         """ initialize ROS subscribers and stores them in a dictionary"""
         # Subscription to encoder data - angular velocity
-        rospy.Subscriber("~input_encoder_data", WheelSpeedStamped, self.encoder_callback)
+        try:
+            rospy.Subscriber("~input_encoder_data", WheelSpeedStamped, self.encoder_callback)
+        except rospy.exceptions.TransportException as e:
+            print("unable to create subscriber transport:WheelSpeedStamped. Will try again Quaternion", e)
+            rospy.Subscriber("~input_encoder_data", Quaternion, self.encoder_callback) # input comes as float64 on .w
 
     def init_publishers(self):
         """ initialize ROS publishers and stores them in a dictionary"""
@@ -34,7 +44,6 @@ class VelocityEstimation(object):
         self.publishers["velocity_estimated"] = rospy.Publisher("~velocity_estimated",
                                                                 Float32,
                                                                 queue_size=1)
-
 if __name__ == "__main__":
     rospy.init_node("velocity_estimation_node")
     VelocityEstimation()
