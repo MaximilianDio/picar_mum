e6a528d3b10b3ce7ef4842c52806ef9d14fc5a5d
mattivahs <matti.vahs@hotmail.de>
2020-01-13 15:49:10 +0100

    Velocity Tuning

--- catkin_ws/src/50_project/race_control/src/trajectory_tracking_node.py
+++ catkin_ws/src/50_project/race_control/src/trajectory_tracking_node.py
@@ -8,7 +8,6 @@ class TrajectoryTrackingNode(object):
     """Race controller ROS node."""
 
     def __init__(self):
-        self.rate = rospy.Rate(100)
         self.publishers = {}
         self.subscribers = {}
 
@@ -18,6 +17,8 @@ class TrajectoryTrackingNode(object):
         self.tracking = VelocityController(10)
         self.cur_time = rospy.Time.now()
         self.start_time = self.cur_time
+        self.desired_vel = None
+        self.desired_angle = None
 
     def init_publishers(self):
         self.publishers["car_cmd"] = rospy.Publisher(
@@ -26,39 +27,50 @@ class TrajectoryTrackingNode(object):
             queue_size=1)
 
     def init_subscribers(self):
-        self.desired_vals = rospy.Subscriber("~des_motor_cmd",
-                                        Point32,
-                                        self.publish_car_cmd(self.cur_time),
-                                        queue_size=1)
+        self.subscribers["desired_vals"] = rospy.Subscriber("~des_motor_cmd",
+                                                            CarCmd,
+                                                            self.update_des_values(),
+                                                            queue_size=1)
+        # TODO check message
+        self.subscribers["vel_meas"] = rospy.Subscriber("~encoder_msg",
+                                                        Point32,
+                                                        self.update_current_velocity(),
+                                                        queue_size=1)
 
-        self.meas_vals = rospy.Subscriber("~encoder_msg",
-                                          Point32,
-                                          self.publish_car_cmd(self.cur_time),
-                                          queue_size=1)
+    def update_des_values(self, message):
+        self.desired_vel = message.velocity
+        self.desired_angle = message.angle
+        # frequency of controller is same as trajectoryplanner
+        self.publish_car_cmd()
+
+    def update_current_velocity(self, message):
+        self.cur_vel = message  # TODO how does the message look like?
 
     def publish_car_cmd(self):
-        while not rospy.is_shutdown():
-            cur_time = rospy.get_rostime() - self.start_time
-            cur_time = cur_time.secs + float(cur_time.nsecs * 1e-9)
-            message = CarCmd()
-            message.header.stamp = rospy.get_rostime()
-            des_vel = self.desired_vals.velocity
-            cur_vel = self.meas_vals.encoder
+        cur_time = rospy.get_rostime() - self.start_time
+        cur_time = cur_time.secs + float(cur_time.nsecs * 1e-9)
+        message = CarCmd()
+        message.header.stamp = rospy.get_rostime()
+        if self.desired_vel is None or self.cur_vel is None:
+            return
 
-            if cur_time > 10:
-                message.velocity = self.tracking.get_velocity_output(cur_vel, des_vel)
-            else:
-                message.velocity = 0.0
+        des_vel = self.desired_vel
+        cur_vel = self.cur_vel
 
-            message.angle = self.desired_vals.angle
-            self.publishers["car_cmd"].publish(message)
-            self.rate.sleep()
+        if cur_time > 10:
+            message.velocity = self.tracking.get_velocity_output(cur_vel, des_vel) # call velocity controller
+        else:
+            message.velocity = 0.0
+
+        message.angle = self.desired_angle
+        self.publishers["car_cmd"].publish(message)
+        self.rate.sleep()
+
 
 def main():
     """Starts the lane control node"""
     rospy.init_node("trajectory_tracking_node")
-    Tracking = TrajectoryTrackingNode()
-    Tracking.publish_car_cmd()
+    TrajectoryTrackingNode()
     rospy.spin()
 
 
---
+++ catkin_ws/src/velocity_control_tuning/src/velocity_control_node.py
@@ -0,0 +1,246 @@
+#!/usr/bin/python
+
+import os
+import time
+import pygame
+import rospy
+from picar_msgs.msg import CarCmd
+from velocity_controller import *
+
+class VelocityControllerTuning(object):
+    """Class to control the vehicle by publishing ROS messages based on keyboard input.
+
+    """
+    K_LEFT = 0
+    K_RIGHT = 1
+    K_UP = 2
+    K_DOWN = 3
+    K_0 = 4
+    K_1 = 5
+    K_2 = 6
+    K_3 = 7
+    K_4 = 8
+    K_5 = 9
+    K_6 = 10
+    K_7 = 11
+    K_8 = 12
+    K_9 = 13
+    SCREEN_SIZE = 640
+
+
+    def __init__(self):
+        pygame.init()
+        # Initialize controller
+        self.controller = VelocityController(5)
+
+        vehicle_name = rospy.get_namespace().strip("/")
+
+        self.velocity = 0.3
+        self.angle = 100.0  # absolute value
+
+        self.screen = pygame.display.set_mode(
+            (VelocityControllerTuning.SCREEN_SIZE, VelocityControllerTuning.SCREEN_SIZE)
+        )
+        pygame.display.set_caption("Keyboard Control of {}"
+                                   "".format(vehicle_name))
+        # font = pygame.font.Font(None, 24)
+
+        file_path = os.path.dirname(__file__)
+        file_path = os.path.join(file_path, "..", "images")
+        icon = pygame.image.load(file_path + "/icon.png")
+        pygame.display.set_icon(icon)
+
+        self.buttons = pygame.image.load(file_path + "/d-pad.png")
+        self.buttons = pygame.transform.scale(
+            self.buttons,
+            (VelocityControllerTuning.SCREEN_SIZE, VelocityControllerTuning.SCREEN_SIZE)
+        )
+
+        button_pressed = pygame.image.load(file_path + "/d-pad-pressed.png")
+        button_pressed = pygame.transform.scale(
+            button_pressed,
+            (VelocityControllerTuning.SCREEN_SIZE, VelocityControllerTuning.SCREEN_SIZE)
+        )
+
+        self.button_image = dict(
+            forward=button_pressed,
+            left=pygame.transform.rotate(button_pressed, 90),
+            right=pygame.transform.rotate(button_pressed, 270),
+            backward=pygame.transform.rotate(button_pressed, 180),
+        )
+
+        # send directly to motor_node. Due to gazebo remapping won't work as
+        # expected.
+        # see:
+        # https://answers.ros.org/question/242581/remapping-the-gazebo-node/
+        self.publisher = rospy.Publisher("motor_node/car_cmd",
+                                         CarCmd,
+                                         queue_size=1)
+
+        # listen to encoder node
+        self.subscriber = rospy.Subscriber("~encoder_msg",
+                                            Point32,
+                                            self.update_current_velocity(),
+                                            queue_size=1)
+
+        self.service = rospy.Service("~set_p_gain",
+                                    SetValue,
+                                    self.set_p_gain)
+
+    def update_current_velocity(self, message):
+        self.cur_vel = message  # TODO how does the message look like?
+
+    def set_p_gain(self, message):
+        self.controller.kp = message.value
+
+    def run(self):
+        """Main logic loop. Should be called periodically in a loop"""
+        running = True
+        while running:
+
+
+
+            self.screen.blit(self.buttons, (0, 0))
+
+            keys = [False] * 14
+
+            keyboard_input = pygame.key.get_pressed()
+
+            self.check_direction_button(keyboard_input, keys)
+            self.check_velocity_button(keyboard_input, keys)
+
+            if keyboard_input[pygame.K_q]:
+                rospy.signal_shutdown("Pygame Quit by user input")
+                break
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rospy.signal_shutdown("Pygame Quit Event")
+                    running = False
+
+            pygame.display.flip()
+            self.pub_message(keys)
+            time.sleep(0.03)
+
+    def check_direction_button(self, keyboard_input, keys):
+        """Checks which arrow keys are pressed"""
+        if keyboard_input[pygame.K_LEFT]:
+            self.screen.blit(self.button_image["left"], (0, 0))
+            keys[self.K_LEFT] = True
+
+        if keyboard_input[pygame.K_RIGHT]:
+            self.screen.blit(self.button_image["right"], (0, 0))
+            keys[self.K_RIGHT] = True
+
+        if keyboard_input[pygame.K_UP]:
+            self.screen.blit(self.button_image["forward"], (0, 0))
+            keys[self.K_UP] = True
+
+        if keyboard_input[pygame.K_DOWN]:
+            self.screen.blit(self.button_image["backward"], (0, 0))
+            keys[self.K_DOWN] = True
+
+    def check_velocity_button(self, keyboard_input, keys):
+        """Checks which number keys are pressed"""
+        if keyboard_input[pygame.K_0]:
+            keys[self.K_0] = True
+
+        if keyboard_input[pygame.K_1]:
+            keys[self.K_1] = True
+
+        if keyboard_input[pygame.K_2]:
+            keys[self.K_2] = True
+
+        if keyboard_input[pygame.K_3]:
+            keys[self.K_3] = True
+
+        if keyboard_input[pygame.K_4]:
+            keys[self.K_4] = True
+
+        if keyboard_input[pygame.K_5]:
+            keys[self.K_5] = True
+
+        if keyboard_input[pygame.K_6]:
+            keys[self.K_6] = True
+
+        if keyboard_input[pygame.K_7]:
+            keys[self.K_7] = True
+
+        if keyboard_input[pygame.K_8]:
+            keys[self.K_8] = True
+
+        if keyboard_input[pygame.K_9]:
+            keys[self.K_9] = True
+
+    def pub_message(self, keys):
+        """Publishes a CarCmd message to control the car.
+
+        Args:
+            keys(list): List of bools corresponding to pressed/unpressed keys
+        """
+
+        car_cmd = CarCmd()
+        car_cmd.velocity = 0.0
+        car_cmd.angle = 0.0
+
+        self.set_velocity_from_keys(keys)
+
+        if keys[self.K_UP]:
+            car_cmd.velocity = self.controller.get_velocity_output(self.cur_vel, self.velocity)
+        if keys[self.K_DOWN]:
+            car_cmd.velocity = -self.controller.get_velocity_output(self.cur_vel, self.velocity)
+        if keys[self.K_LEFT]:
+            car_cmd.angle = self.angle # angle will be clamped by the car itself
+        if keys[self.K_RIGHT]:
+            car_cmd.angle = -self.angle # angle will be clamped by the car itself
+
+
+
+        """ prints angle and data for debugging """
+        #print("angle = " + str(car_cmd.angle))
+        #print("velocity = " + str(car_cmd.velocity))
+
+        self.publisher.publish(car_cmd)
+
+    def set_velocity_from_keys(self, keys):
+        """Sets the member 'velocity' according to arrow keys pressed
+
+        Args:
+            keys (list): List of bools corresponding to pressed/unpressed keys
+        """
+        if keys[self.K_0]:
+            self.velocity = 1.0
+        if keys[self.K_1]:
+            self.angle = input("set angle: ")
+            #self.velocity = 0.1
+        if keys[self.K_2]:
+            self.velocity = 0.2
+        if keys[self.K_3]:
+            self.velocity = 0.3
+        if keys[self.K_4]:
+            self.velocity = 0.4
+        if keys[self.K_5]:
+            self.velocity = 0.5
+        if keys[self.K_6]:
+            self.velocity = 0.6
+        if keys[self.K_7]:
+            self.velocity = 0.7
+        if keys[self.K_8]:
+            self.velocity = 0.8
+        if keys[self.K_9]:
+            self.velocity = 0.9
+
+
+def main():
+    """Inits the ROS node and KeyboardController."""
+    rospy.init_node("keyboard_control_node")
+
+    node = KeyboardController()
+    try:
+        node.run()
+    except rospy.ROSInitException as error:
+        rospy.signal_shutdown("{}".format(error))
+
+
+if __name__ == "__main__":
+    main()
