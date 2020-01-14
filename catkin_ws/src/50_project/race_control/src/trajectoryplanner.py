35ebe40397aa447efc016bddc3eda2269b74ad79
mattivahs <matti.vahs@hotmail.de>
2020-01-14 17:30:41 +0100

    New Trajectory + launch file

--- catkin_ws/src/50_project/race_control/src/trajectoryplanner.py
+++ catkin_ws/src/50_project/race_control/src/trajectoryplanner.py
@@ -11,7 +11,7 @@ class OvertakingTrajectory(object):
     def __init__(self):
         file_path = os.path.join(rospkg.RosPack().get_path("race_control"),
                                             "src",
-                                            "V0to0.csv")
+                                            "Overtake.csv")
         traj = np.genfromtxt(file_path, delimiter=',')
         self.time = traj[0, :]
         self.velocity = traj[1, :]
@@ -23,7 +23,7 @@ class OvertakingTrajectory(object):
 
     def get_feedforward_control(self, current_time):
         if current_time > self.time[len(self.time)-1]:
-            return 0, 0
+            return 0.0, 0.0
         else:
             velocity_interp = interp1d(self.time, self.velocity)
             delta_interp = interp1d(self.time, self.delta)
