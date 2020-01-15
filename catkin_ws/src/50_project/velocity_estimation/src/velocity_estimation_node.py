06e27ba801925eb8886c3222fe0529a0ca22553c
mattivahs <matti.vahs@hotmail.de>
2020-01-15 12:42:58 +0000

    Kalman Filter for velocity estimation

--- catkin_ws/src/50_project/velocity_estimation/src/velocity_estimation_node.py
+++ catkin_ws/src/50_project/velocity_estimation/src/velocity_estimation_node.py
@@ -10,7 +10,8 @@ class VelocityEstimation(object):
     def __init__(self):
         self.rate = rospy.Rate(100)  # TODO publish hertz rate anpassen
         self.publishers = {}
-        self.velocity_estimator = VelocityEstimator(radius=Wheel.wheel_diameter/2)
+        self.velocity_estimator = VelocityEstimator(Wheel.wheel_diameter/2)
+        self.encoder_data = [0.0, 0.0]
 
         # register all publishers
         self.init_subscribers()
@@ -18,19 +19,27 @@ class VelocityEstimation(object):
         self.init_publishers()
 
     def encoder_callback(self, input_msg):
-        velocity = Float32()
+        self.encoder_data = input_msg
+        #velocity = Float32()
 
-        velocity.data = self.velocity_estimator.getCOMvel([input_msg.rear_left, input_msg.rear_right])
+        #velocity.data = self.velocity_estimator.getCOMvel([input_msg.rear_left, input_msg.rear_right])
 
         # velocity.header.stamp = rospy.Time.now()
-        self.publishers["velocity_estimated"].publish(velocity)
-        self.rate.sleep()
+        #self.publishers["velocity_estimated"].publish(velocity)
+        #self.rate.sleep()
+
+    def estimate_velocity(self, imudata):
+        velocity_est = Float32()
+        velocity_est.data = self.velocity_estimator.getCOMvel(self.encoder_data, imudata)
+        velocity_est.header.stamp = rospy.Time.now()
+        self.publishers["velocity_estimated"].publish(velocity_est)
 
     def init_subscribers(self):
         """ initialize ROS subscribers and stores them in a dictionary"""
         # Subscription to encoder data - angular velocity
 
         rospy.Subscriber("~input_encoder_data", WheelSpeedStamped, self.encoder_callback)
+        rospy.Subscriber("~imu", Imu, self.estimate_velocity)
 
 
     def init_publishers(self):
--- catkin_ws/src/50_project/velocity_estimation/src/velocity_estimator.py
+++ catkin_ws/src/50_project/velocity_estimation/src/velocity_estimator.py
@@ -1,3 +1,6 @@
+import numpy as np
+import rospy
+
 class VelocityEstimator(object):
     """
     Converts Encoderdata to COM Velocity
@@ -6,9 +9,39 @@ class VelocityEstimator(object):
     def __init__(self, radius):
         self.radius_wheel = radius
 
+        # Parameters for Kalman Filter
+        self.Q = np.array([[0.001, 0.0], [0.0, 0.003]])
+        self.R = 0.03
+
+        self.x_hat = np.array([0, 0]).T
+        self.P = np.zeros(2, 2)
+        self.cur_time = rospy.get_rostime()
+        self.cur_time = self.cur_time.secs + float(self.cur_time.nsecs * 1e-9)
+        self.last_time = self.cur_time
+
     def getVelocity(self, rawdata):
         return [x*self.radius_wheel for x in rawdata]
 
-    def getCOMvel(self, rawdata):
+    def getCOMvel(self, rawdata, imu_data):
+        ax = imu_data.linear_accelaration.x  # Float64
         velocity_est = self.getVelocity(rawdata)
-        return float(velocity_est[0] + (velocity_est[1] - velocity_est[0]) / 2)
+        velocity_com = float(velocity_est[0] + (velocity_est[1] - velocity_est[0]) / 2)
+
+        dt = self.cur_time - self.last_time
+        # Prediction step
+        A = np.array([[1, -dt], [0, 1]])
+        self.x_hat = np.dot(A, self.x_hat) + np.dot(np.array([1, 0]).T, ax)
+
+        # New covariance matrix
+        self.P = A * self.P * A.T + self.Q
+
+        # measurement update
+        y = velocity_com - self.x_hat[0]
+
+        # Kalman Gain
+        K = self.P * np.array([1, 0]).T / (np.array([1, 0]) * self.P * np.array([1, 0]).T + self.R)
+        self.x_hat = self.x_hat + K * y
+
+        self.last_time = self.cur_time
+
+        return self.x_hat[0]
