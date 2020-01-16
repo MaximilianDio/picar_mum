9adda721f423e98597195fa9d5d2899f18b4b427
Timo <timo.schmidt@tuhh.de>
2020-01-16 15:51:46 +0100

    steering controller implemented and debugged

--- catkin_ws/src/50_project/steering_control/include/steering_control/path_tracking_ff.py
+++ catkin_ws/src/50_project/steering_control/include/steering_control/path_tracking_ff.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import numpy as np
+
 from picar.parameters import Picar, Wheel
 
 
@@ -8,12 +9,12 @@ class PathTrackingFF(object):
     def __init__(self):
 
         self.xLA = 1  # look a head error
-        self.Kp = 1 # init Kp
+        self.Kp = 1  # init Kp
 
-        #Model based params
+        # Model based params
         self.mass = Picar.mass
         self.Inertia = Picar.inertia_z
-        self.a = Picar.length - Picar.cog_x   # a is length front <- cog
+        self.a = Picar.length - Picar.cog_x  # a is length front <- cog
         self.b = Picar.cog_x  # b is length rear <- cog COS in rear axis
         self.l = Picar.length
 
@@ -23,36 +24,41 @@ class PathTrackingFF(object):
 
         # constants
         self.C0 = self.C_f + self.C_r
-        self.C1 = self.a*self.C_f - self.b*self.C_r
-        self.C2 = self.a**2*self.C_f - self.b**2*self.C_r
+        self.C1 = self.a * self.C_f - self.b * self.C_r
+        self.C2 = self.a ** 2 * self.C_f - self.b ** 2 * self.C_r
 
+    def get_control_output(self, inputdata):
+        """
 
-    def get_control_output(self):
-        #discrete form of controller
-        U_x =                   
-        error=
-        delta_phi=
-        kappa=
+        :param inputData: object of class ControlerValues
+        :return: steering angle of vehicle
+        """
+        # discrete form of controller
+        u_x = inputdata.u_x
+        error = inputdata.error
+        delta_psi = inputdata.delta_psi
+        kappa = inputdata.kappa
+
         # feedback steering angle
-        delta_fb = - self.Kp * error-self.Kp*self.xLA*delta_phi
-        
+        delta_fb = - self.Kp * error - self.Kp * self.xLA * delta_psi
+
         # feedforward steering angle due to curvature and longitudinal velocity
-        g_ff = self.mass * U_x **2 / self.l * ((self.b * self.C_r + self.a * self.C_f * (self.Kp * self.xLA - 1)) / (
-                    self.C_r * self.C_f)) + self.l - self.b * self.Kp * self.xLA
+        g_ff = self.mass * u_x ** 2 / self.l * ((self.b * self.C_r + self.a * self.C_f * (self.Kp * self.xLA - 1)) / (
+                self.C_r * self.C_f)) + self.l - self.b * self.Kp * self.xLA
         delta_ff = g_ff * kappa
 
         # total steering angle
-        delta =  delta_ff + delta_fb
+        delta = delta_ff + delta_fb
 
         return delta
 
-    def format_control_inputs(self,curve_point, velocity_estimated):
+    def format_control_inputs(self, curve_point, velocity_estimated):
         # input for controller
         output_data = ControllerValues()
 
         output_data.error = curve_point.y
         output_data.delta_psi = np.arctan(curve_point.slope)
-        output_data.kappa = 1/curve_point.cR
+        output_data.kappa = 1 / curve_point.cR
 
         # data from encoders
         output_data.u_x = velocity_estimated
@@ -72,11 +78,23 @@ class PathTrackingFF(object):
         if Kp is not None:
             self.Kp = Kp
 
+
 class ControllerValues(object):
-    def __init__(self,):
+    def __init__(self):
 
-        self.error = 0.0        # lateral error
-        self.delta_psi = 0.0    # relative orientation error
+        self.error = 0.0  # lateral error
+        self.delta_psi = 0.0  # relative orientation error
 
-        self.u_x = 0.0          # current longitudinal velocity
-        self.kappa = 0.0        # current curvature
+        self.u_x = 0.0  # current longitudinal velocity
+        self.kappa = 0.0  # current curvature
+
+    def set_values(self, error, delta_psi, u_x, kappa):
+
+        if error is not None:
+            self.error = error
+        if delta_psi is not None:
+            self.delta_psi = delta_psi
+        if u_x is not None:
+            self.u_x = u_x
+        if kappa is not None:
+            self.kappa = kappa
