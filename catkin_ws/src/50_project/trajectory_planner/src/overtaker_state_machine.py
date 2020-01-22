2e7739bc746422bb79b8a418d1fa0ef16aede867
mattivahs <matti.vahs@hotmail.de>
2020-01-22 15:14:11 +0100

    Include trajectories in state machine

--- catkin_ws/src/50_project/trajectory_planner/src/overtaker_state_machine.py
+++ catkin_ws/src/50_project/trajectory_planner/src/overtaker_state_machine.py
@@ -1,7 +1,8 @@
 from distance_controller import PIDDistanceController
 from steering_controller import PathTrackingFF
 from velocity_picker import VelocityPicker
-import numpy as np
+from trajectoryplanner import OvertakingTrajectory
+from picar.parameters import Picar
 
 
 class OvertakeStateMachine:
@@ -17,6 +18,7 @@ class OvertakeStateMachine:
 
         # updated by velocity estimator node
         self.own_velocity_est = 0.0
+        self.vel_reference = params_dict["vel_reference"]  # desired velocity for state 1,2
 
         self.time = 0.0  # initial time (starts when state machine gets first pacemaker message)
         self.overtake_start_time = 0.0
@@ -28,10 +30,9 @@ class OvertakeStateMachine:
         # distance when to switch from state 2 to 3 or 3*
         self.min_dist_obstacle = params_dict["min_dist_obstacle"]  # defined in yaml file
 
-
         self.overtake_time = None  # calculated during transition to state 3
 
-        # calculated in transition to state 3
+        # calculated in transition to state 3 and 5
         self.t_trajectory = None
 
         # TODO init all controller types and values (as class objects)
@@ -63,7 +64,8 @@ class OvertakeStateMachine:
         self.velocity_control_3star = PIDDistanceController(Kp_distance, Kd_distance, Ki_distance)
 
         # -- TRAJECTORY PLANNER FOR OVERTAKING
-        # TODO MATTI: init class overting trajectory
+        self.trajectory = OvertakingTrajectory()
+        self.mappings = Picar()
 
         # return values for car command
         self.des_velocity = 0.0
@@ -173,7 +175,6 @@ class OvertakeStateMachine:
                 # stay in state
                 return
             else:
-
                 self.init_state_3_transition()
                 self.current_state = "3"
                 return
@@ -182,15 +183,15 @@ class OvertakeStateMachine:
         """ initialize necessary information before going to state 3 - e.g. calculate necessary overtake time,
         trajectory time ... """
 
-        # TODO MATTI: change
-        self.des_velocity = 0.8
-        self.des_angle = 0.0
+        # desired initial values
+        self.des_velocity = self.trajectory.velocity[0]
+        self.des_angle = self.trajectory.angle[0]
 
         # use overtake_start_time to calculate delta time (e.g. time spent in state)
         self.overtake_start_time = self.time
 
-        # TODO MATTI: calculate necessary overtake time (based on velocity and position of own vehicle and obstacle)
-        self.t_trajectory = 5.0  # TODO: calculate necessary time to fulfill open loop maneuver (only trajectory)
+        # necessary overtake time (based on velocity and position of own vehicle and obstacle)
+        self.t_trajectory = self.trajectory.time[len(self.trajectory.time) - 1]
 
     def state_3(self):
         """ open loop controlled overtaking maneuver, to other line"""
@@ -200,11 +201,12 @@ class OvertakeStateMachine:
         print "time spent in state 3: " + str(state_time)
 
         # run controller
-        # TODO MATTI: open loop control with overtaking time
+        # open loop control with overtaking time
+        open_loop_velocity, open_loop_angle = self.trajectory.get_feedforward_control(state_time)
 
-        # TODO: MATTI: -update desired angle and velocity
-        self.des_velocity = 0.0
-        self.des_angle = 0.0
+        # update desired angle and velocity according to mappings
+        self.des_velocity = self.mappings.get_velocity(open_loop_velocity)
+        self.des_angle = self.mappings.get_angle(open_loop_angle)
 
         # change state if needed
         if state_time < self.t_trajectory:
@@ -212,4 +214,5 @@ class OvertakeStateMachine:
             return
         else:
             self.current_state = "1"
+            self.switch_params["overtake"] = False
             return
