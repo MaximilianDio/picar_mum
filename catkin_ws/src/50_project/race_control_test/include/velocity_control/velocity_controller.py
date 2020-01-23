9fae3d68ffb2219f9d653fea7fda8dd9e991e042
paulo-95 <paul.over@tuhh.de>
2020-01-23 23:50:24 +0100

    velocity_controller.py implemtend part 2

---
+++ catkin_ws/src/50_project/race_control_test/include/velocity_control/velocity_controller.py
@@ -0,0 +1,71 @@
+import rospy
+
+
+class VelocityController(object):
+
+    def __init__(self, kp, kd):
+        """
+        Args:
+            Kp (list of proportional gains [Velocity, Steering])
+            Kd (list of derivative gains [Velocity, Steering])
+        """
+        self.kp = kp
+        self.kd = kd
+        self.current_vel = 0.0
+        self.desired_vel = 0.0
+        self.last_vel = self.current_vel
+
+        self.init_time = rospy.get_rostime()
+        self.init_time = self.init_time.secs + float(self.init_time.nsecs * 1e-9)
+
+        self.cur_time = rospy.get_rostime()
+        self.cur_time = self.cur_time.secs + float(self.cur_time.nsecs * 1e-9)
+
+        self.cur_time = self.cur_time - self.init_time
+
+        self.last_time = self.cur_time
+        self.error = 0.0
+        self.error_integral = 0.0
+        self.sat = 2.5
+        self.last_control = 0.0
+
+
+    def get_velocity_output(self, des_vel, meas):
+
+        # ---------------- update attributes -------------------------
+        self.cur_time = rospy.get_rostime()
+        self.cur_time = self.cur_time.secs + float(self.cur_time.nsecs * 1e-9)
+
+        self.cur_time = self.cur_time - self.init_time
+
+        self.current_vel = meas
+        self.desired_vel = des_vel
+
+        # ---------------- assemble control_values -------------------------
+        if abs(des_vel) < 0.2:
+            self.last_control = 0.0
+
+        dt = self.cur_time - self.last_time
+        dv = self.current_vel - self.last_vel
+
+        self.error = self.current_vel - self.desired_vel
+
+
+        # ---------------- compute vel_cmd -------------------------
+        vel_output = self.last_control + (- self.kp * self.error - self.kd * (dv / dt))
+
+        self.last_control = vel_output
+        self.last_time = self.cur_time
+        self.last_vel = self.current_vel
+
+        # ----------------Limit Vel CMD-------------------------
+
+        if vel_output < 0:
+            print("Velocity negativ!!!")
+            vel_output = 0
+
+        return vel_output
+
+    def update_gains(self, kp, kd):
+        self.kp = kp
+        self.kd = kd
--- catkin_ws/src/50_project/race_control_test/src/race_control_test_node.py
+++ catkin_ws/src/50_project/race_control_test/src/race_control_test_node.py
@@ -5,6 +5,7 @@ from picar_msgs.srv import SetValue, SetBool, SetSteeringParametersList, SetVelo
 from std_msgs.msg import Float32
 from picar_msgs.msg import CarCmd, MsgCurvePoint2D
 from picar_common.picar_common import get_param, get_config_file_path, set_param
+import numpy as np
 
 # import controllers
 from steering_control import steering_controller
@@ -28,6 +29,14 @@ class RaceControlNode:
         self.own_velocity_est = 0.0
         self.curve_point = None  # CurvePointMessage
 
+        self.init_time = rospy.get_rostime()
+        self.init_time = self.init_time.secs + float(self.init_time.nsecs * 1e-9)
+
+        self.cur_time = rospy.get_rostime()
+        self.cur_time = self.cur_time.secs + float(self.cur_time.nsecs * 1e-9)
+
+        self.cur_time = self.cur_time - self.init_time
+
         # import parameters from config yaml files
         config_file_name = get_param("~config_file_name", "default")
 
@@ -42,7 +51,7 @@ class RaceControlNode:
         # read the config parameters form .yaml file
         self.setup_params(config_file_path)
 
-        self.DEBUG = True
+        self.DEBUG = False
 
         # register all publishers
         self.init_subscribers()
@@ -71,10 +80,8 @@ class RaceControlNode:
 
     def init_subscribers(self):
         """ initialize ROS subscribers and stores them in a dictionary"""
-        # point on curve with position and circle
-        rospy.Subscriber("~curve_point", MsgCurvePoint2D, self.update_curved_point)
         # Kalmanfilter output - get velocity est.
-        rospy.Subscriber("~velocity_estimated", Float32, self.update_own_velocity)
+        rospy.Subscriber("~velocity_estimated", Float32, self.update_own_velocity, queue_size=1)
 
     def init_publishers(self):
         """ initialize ROS publishers and stores them in a dictionary"""
@@ -83,22 +90,8 @@ class RaceControlNode:
 
     def init_services(self):
 
-        self.services["set_vel_reference"] = rospy.Service(
-            "~set_vel_reference",
-            SetValue,
-            self.service_vel_reference)
 
-        self.services["set_switching_radius"] = rospy.Service(
-            "~set_switching_radius",
-            SetValue,
-            self.service_switching_radius)
 
-        # ---------------- controller params -------------------------
-
-        self.services["update_steering_parameters"] = rospy.Service(
-            "~update_steering_parameters",
-            SetSteeringParametersList,
-            self.service_steering_params)
 
         self.services["update_velocity_parameters"] = rospy.Service(
             "~update_velocity_parameters",
@@ -107,39 +100,11 @@ class RaceControlNode:
 
     def init_controller(self):
 
-        # ---------------- init controllers -------------------------
-        # steering control
-        self.steering_control = steering_controller.PathTrackingFF(self._params["Kp_steering"],
-                                                                   self._params["Kp_c_steering"],
-                                                                   self._params["xLA_steering"])
-        # velocity picker
-        self.velocity_picker = velocity_picker.VelocityPicker(self._params["vel_reference"],
-                                                              self._params["switch_bound"])
-
         # velocity control
         self.velocity_control = velocity_controller.VelocityController(self._params["Kp_velocity"],
                                                                        self._params["Kd_velocity"])
 
     # ---------------- controller params -------------------------
-
-    def service_vel_reference(self, request):
-        """Sets values via service"""
-        self.velocity_picker.minimal_velocity = request.value
-        print ("<<<<< Hebel auf den Tisch >>>>>")
-        return 1
-
-    def service_switching_radius(self, request):
-        """ set switching radius of velocity picker"""
-        self.velocity_picker.switch_bound = request.value
-        print("%%%%% Schnipp Schnapp %%%%%")
-        return 1
-
-    def service_steering_params(self, request):
-        self.steering_control.Kp = request.Kp
-        self.steering_control.Kp_c = request.Kp_c
-        self.steering_control.xLA = request.xLA
-        print ("<------ Heiz Heiz ------> ")
-        return 1
 
     def service_velocity_params(self, request):
         self.velocity_control.kp = request.Kp
@@ -149,40 +114,34 @@ class RaceControlNode:
 
     # ---------------------- Topic Callbacks -----------------------------
 
-    def update_curved_point(self, message):
-        # message of type MsgCurvePoint2D
-        curve_point = message
-        # update curvepoint
-        if curve_point.x == DEFAULT_FALSE_FLOAT_VALUE or curve_point.y == DEFAULT_FALSE_FLOAT_VALUE:
-            self.curve_point = None
-        else:
-            self.curve_point = curve_point
-
-        self.control_picar()
-
     def update_own_velocity(self, message):
         self.own_velocity_est = message.data
 
+        self.control_picar()
+
     def control_picar(self):
 
-        if self.curve_point is None:
-            angle = 0
-            velocity = 0
+        self.cur_time = rospy.get_rostime()
+        self.cur_time = self.cur_time.secs + float(self.cur_time.nsecs * 1e-9)
 
+        self.cur_time = self.cur_time - self.init_time
+
+        if self.cur_time < 8:
+            des_velocity = 0
         else:
-            angle = self.steering_control.get_steering_output(self.curve_point, self.own_velocity_est)
+            des_velocity = 0.1 * np.sin(0.05 * np.pi * 2 * self.cur_time) + 0.7
 
-            des_velocity = self.velocity_picker.get_velocity(self.curve_point)
+        angle = 0
+        print "velocity picked: " + str(des_velocity)
+        velocity = self.velocity_control.get_velocity_output(des_velocity, self.own_velocity_est)
+        print "velocity cmd: " + str(velocity)
 
-            #velocity = des_velocity
-
-            velocity = self.velocity_control.get_velocity_output(des_velocity, self.own_velocity_est)
         try:
             if self.DEBUG == True:
                 print "-----------------------------------------------------------------------------------"
                 print "-----------------------------------------------------------------------------------"
                 print "-----------------------" + str(
-                       rospy.get_rostime()) + "----------------------------"  # TODO change time
+                    rospy.get_rostime()) + "----------------------------"  # TODO change time
                 print "curve point: x: " + str(self.curve_point.x) + " y: " + str(self.curve_point.y)
                 print "own velocity: " + str(self.own_velocity_est)
                 print "angle cmd: " + str(angle)
